b3a2d1651c5510de19aeb72ae632fae7a6331b60
Marijn Ophorst <marijn@monomelodies.nl>
2018-03-24 08:04:51 +0100

    docs

--- src/PluginUnavailableException.php
+++ src/PluginUnavailableException.php
@@ -4,6 +4,9 @@ namespace Dbmover\Core;
 
 use DomainException;
 
+/**
+ * Thrown if a plugin was requested, but it is not available.
+ */
 class PluginUnavailableException extends DomainException
 {
 }
