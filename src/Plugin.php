02eb4abdd3685f32d633b3fcd32885f05ec199c5
Marijn Ophorst <marijn@monomelodies.nl>
2018-03-24 08:04:18 +0100

    docs

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,38 +4,72 @@ namespace Dbmover\Core;
 
 abstract class Plugin implements PluginInterface
 {
+    /** @var Dbmover\Core\Loader */
     protected $loader;
+    /** @var array */
     protected $statements = [];
+    /** @var array */
     protected $deferredStatements = [];
+    /** @var string */
     public $description = 'DbMoving...';
 
+    /**
+     * @param Dbmover\Core\Loader $loader
+     * @return void
+     */
     public function __construct(Loader $loader)
     {
         $this->loader = $loader;
     }
 
+    /**
+     * @param string $sql
+     * @return string The same SQL, with handled statements stripped.
+     */
     public function __invoke(string $sql) : string
     {
         return $sql;
     }
 
-    public function addOperation(string $sql)
+    /**
+     * Add an SQL operation to the pool.
+     *
+     * @param string $sql
+     * @return void
+     */
+    public function addOperation(string $sql) : void
     {
         $this->statements[] = $sql;
     }
 
-    public function defer(string $sql)
+    /**
+     * Defer an SQL statement for later handling.
+     *
+     * @param string $sql
+     * @return void
+     */
+    public function defer(string $sql) : void
     {
         $this->deferredStatements[] = $sql;
     }
 
-    public function persist()
+    /**
+     * Persist all default statements to the Loader.
+     *
+     * @return void
+     */
+    public function persist() : void
     {
         if ($this->statements) {
             $this->loader->addOperation($this->description, $this->statements);
         }
     }
 
+    /**
+     * Persist all deferred statements to the loader.
+     *
+     * @return void
+     */
     public function __destruct()
     {
         if ($this->deferredStatements) {
