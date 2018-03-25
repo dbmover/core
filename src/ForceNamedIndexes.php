79122099fb0c7bf16ced5f4e15bb62e718091fb0
Marijn Ophorst <marijn@monomelodies.nl>
2018-03-25 11:34:07 +0200

    move to core

---
+++ src/ForceNamedIndexes.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @package Dbmover
+ * @subpackage Core
+ */
+
+namespace Dbmover\Core;
+
+class ForceNamedIndexes extends Plugin
+{
+    const DESCRIPTION = 'Rewriting indexes to named indexes...';
+
+    public function __invoke(string $sql) : string
+    {
+        if (preg_match_all(Indexes::REGEX, $sql, $matches, PREG_SET_ORDER)) {
+            foreach ($matches as $match) {
+                if (!strlen(trim($match[2]))) {
+                    $name = preg_replace("@[\W_]+@", '_', "{$match[3]}_".strtolower($match[5])).'_idx';
+                    $_sql = str_replace('INDEX ON', "INDEX $name ON", $match[0]);
+                    $sql = str_replace($match[0], $_sql, $sql);
+                }
+            }
+        }
+        return $sql;
+    }
+}
+
