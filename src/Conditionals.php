366ab9aa1ade607986ce3fe6f4847cb5c9ad2a0a
Marijn Ophorst <marijn@monomelodies.nl>
2018-03-25 11:33:47 +0200

    move to core

---
+++ src/Conditionals.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @package Dbmover
+ * @subpackage Core
+ * @subpackage Conditionals
+ */
+
+namespace Dbmover\Core;
+
+/**
+ * Gather all conditionals and optionally wrap them in a "lambda".
+ */
+class Conditionals extends Plugin
+{
+    const DESCRIPTION = 'Executing conditional blocks...';
+
+    public function __invoke(string $sql) : string
+    {
+        foreach ($this->extractOperations('@^IF.*?^END IF;$@ms', $sql) as $if) {
+            $tmp = 'tmp_'.md5(microtime(true));
+            $code = $this->wrap($if[0]);
+            $this->defer($code);
+            $this->addOperation($code);
+        }
+        return $sql;
+    }
+
+    protected function wrap(string $sql) : string
+    {
+        return $sql;
+    }
+}
+
