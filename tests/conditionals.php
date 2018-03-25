108bea6654aeb2faf4058e74531f92c7e4974e07
Marijn Ophorst <marijn@monomelodies.nl>
2018-03-25 14:24:08 +0200

    test for conditionals plugin

---
+++ tests/conditionals.php
@@ -0,0 +1,24 @@
+<?php
+
+use Gentry\Gentry\Wrapper;
+use Dbmover\Core\Conditionals;
+use Dbmover\Core\Loader;
+
+/** Testsuite for Dbmover\Core\Conditionals */
+return function () : Generator {
+    $loader = new Loader('mysql:dbname=dbmover_test', ['user' => 'dbmover_test', 'pass' => 'moveit'], true);
+    $object = Wrapper::createObject(Dbmover\Core\Conditionals::class, $loader);
+    /** Conditionals get extracted */
+    yield function () use ($object) {
+        $result = $object->__invoke(<<<EOT
+
+IF foo = 'bar' THEN
+
+END IF;
+EOT
+        );
+        assert(trim($result) === '');
+    };
+
+};
+
