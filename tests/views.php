a92b3fdadda6c9b7dc1f08ca0fd4d96a35f9ccc0
Marijn Ophorst <marijn@sensimedia.nl>
2023-05-22 14:28:15 +0200

    fix test

--- tests/views.php
+++ tests/views.php
@@ -7,7 +7,7 @@ use Dbmover\Core\Views;
 /** Testsuite for Dbmover\Core\Views */
 return function () : Generator {
     $loader = new Loader('mysql:dbname=dbmover_test', ['user' => 'dbmover_test', 'pass' => 'moveit'], true);
-    $views = Wrapper::createObject(Views::class, $loader);
+    $views = new Wrapper(new Views($loader));
     /** Views get correctly stripped from a schema */
     yield function () use ($views) {
         $result = $views->__invoke('CREATE VIEW foo AS SELECT * FROM bar;');
