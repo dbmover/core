30e0a1eb0b83dd986c44eb254a50f8c637a54165
Marijn Ophorst <marijn@sensimedia.nl>
2023-05-22 14:27:56 +0200

    fix test

--- tests/explicitdrop.php
+++ tests/explicitdrop.php
@@ -6,7 +6,7 @@ use Dbmover\Core\Loader;
 /** Testsuite for Dbmover\Core\ExplicitDrop */
 return function () : Generator {
     $loader = new Loader('mysql:dbname=dbmover_test', ['user' => 'dbmover_test', 'pass' => 'moveit'], true);
-    $object = Wrapper::createObject(Dbmover\Core\ExplicitDrop::class, $loader);
+    $object = new Wrapper(new Dbmover\Core\ExplicitDrop($loader));
     /** Explicit drop statements get extracted */
     yield function () use ($object) {
         $result = $object->__invoke('DROP TABLE blarps;');
